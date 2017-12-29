a0e6729ad02218c4301675bbb3e21e4834a55f1a
JebLin <Sword52888@163.com>
2017-12-29 16:30:56 +0800

    the first spring boot project

---
+++ first-boot/src/main/java/indi/sword/springboot/MyController.java
@@ -0,0 +1,16 @@
+package indi.sword.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
+
+}
